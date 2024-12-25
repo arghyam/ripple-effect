3496936d3e2d2dacdb3c565e9cf8d659f110378a
Hp <kevalkanpariya5051@gmail.com>
2024-12-25 14:47:55 +0530

    SERVER URL added in .env file

--- web/src/api/apiService.ts
+++ web/src/api/apiService.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
+import * as dotenv from 'dotenv-flow'
+dotenv.config({ path: './' })
 
-const API_URL = 'https://ripple-api.buckets.growsoc.arpan.xyz/api/user'; // Replace with your API URL
+
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+const API_URL = `${SERVER_URL}/api/user`; // Replace with your API URL
 
 interface IngredientData2 {
   id: string;
--- web/src/api/authApiService.ts
+++ web/src/api/authApiService.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv-flow'
+dotenv.config({ path: '../../' })
 
-const API_URL = 'https://ripple-api.buckets.growsoc.arpan.xyz/api/auth';
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+const API_URL = `${SERVER_URL}/api/auth`; // Replace with your API URL
+
 
 interface RegisterUserReq {
   name: string;
