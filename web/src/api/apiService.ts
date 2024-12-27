9d026acc3add13069e97dbdb5efe9dd203993753
Hp <kevalkanpariya5051@gmail.com>
2024-12-27 20:59:26 +0530

    chore: remove unused variables and files for code cleanup

--- web/src/api/apiService.ts
+++ web/src/api/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as dotenv from 'dotenv-flow'
-dotenv.config({ path: './' })
+dotenv.config({ path: '../../' })
 
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
